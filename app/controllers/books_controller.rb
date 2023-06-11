1d906bdcf369c04f95f52f43fe20f65d674a44cc
EC2 Default User <ec2-user@ip-172-31-3-152.ap-northeast-1.compute.internal>
2023-06-11 12:26:00 +0000

    debug

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,71 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+  def show
+    @book = Book.find(params[:id])
+    @books = Book.new
+    @book_comment = BookComment.new
+
+    @book_detail = Book.find(params[:id])
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
+      current_user.view_counts.create(book_id: @book_detail.id)
+    end
+  end
+
+  def index
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).
+      sort_by {|x|
+        x.favorited_users.includes(:favorites).where(created_at: from...to).size
+      }.reverse
+    # @books = Book.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    user = User.find(book.user_id)
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+
+end
---
+++ test/models/roomrail_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class RoomrailTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
